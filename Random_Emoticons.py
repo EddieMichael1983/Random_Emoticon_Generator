b76411b08b5a6a448a638619fd6530e424280d8b
EddieMichael1983 <46540863+EddieMichael1983@users.noreply.github.com>
2019-03-21 19:19:12 -0700

    Create Random_Emoticons.py

---
+++ Random_Emoticons.py
@@ -0,0 +1,10 @@
+import random
+eyes = [':', '8', 'x']
+noses = ['-', '~', 'っ']
+mouths = ['D', 'O', 'o', '\)']
+
+eye = random.choice(eyes)
+nose = random.choice(noses)
+mouth = random.choice(mouths)
+
+print(eye + nose + mouth)
