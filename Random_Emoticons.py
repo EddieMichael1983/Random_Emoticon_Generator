9200fe1e4705c124712ad05a3481303e1676583e
EddieMichael1983 <46540863+EddieMichael1983@users.noreply.github.com>
2019-03-21 19:30:14 -0700

    Update Random_Emoticons.py

--- Random_Emoticons.py
+++ Random_Emoticons.py
@@ -8,3 +8,4 @@ nose = random.choice(noses)
 mouth = random.choice(mouths)
 
 print(eye + nose + mouth)
+#concatenation using plus signs allows us to print each string next to each other, assembling the emoticon on one line
