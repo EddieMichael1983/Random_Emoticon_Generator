5d29ce9d354d7b9572594175c8cc6fe179066986
EddieMichael1983 <46540863+EddieMichael1983@users.noreply.github.com>
2019-03-21 19:29:07 -0700

    Update Random_Emoticons.py

--- Random_Emoticons.py
+++ Random_Emoticons.py
@@ -1,11 +1,10 @@
 import random
 eyes = [':', '8', 'x', ';']
-noses = ['-', '~', 'っ', '^', '\'']
-mouths = ['D', 'O', 'o', '\)', '\(', '<', 'v', 'P']
+noses = ['-', '~', 'っ', '^', '\'', ''] #empty string at end will give program an option to randomly produce emoticon without a nose
+mouths = ['D', 'O', 'o', ')', '(', '<', 'v', 'P']
 
 eye = random.choice(eyes)
 nose = random.choice(noses)
 mouth = random.choice(mouths)
 
 print(eye + nose + mouth)
-#concatenation using plus signs allows us to print each string next to each other, assembling the emoticon on one line
